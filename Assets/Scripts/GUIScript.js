612c5c6f0e3e96d17886fc9b7e38dfd7bd1195cf
Yuqi Hou <yuqi.hou@gmail.com>
2016-02-22 04:01:58 -0500

    Added instruction text

---
+++ Assets/Scripts/GUIScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+var MenuSkin : GUISkin;
+ 
+function OnGUI() {
+	GUI.skin = MenuSkin;
+	GUI.skin.box.wordWrap = true;
+    GUI.BeginGroup(new Rect(Screen.width/2-100,Screen.height/2-220,200,100));
+	GUI.Box(Rect(0,0,200,60),"Press up or down to move the bullet and press space to fire. The goal is to hit the target.");
+	GUI.EndGroup ();
+	Debug.Log(Screen.width/2-150);
+	Debug.Log(Screen.height/2-150);
+}

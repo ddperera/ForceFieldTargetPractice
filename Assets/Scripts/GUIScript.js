47e1b3594771f9eca94b4e33ed3ed3cc7e63cbf5
Johnathon Root <jroot@mit.edu>
2016-02-22 15:49:31 -0500

    Fixed the Sprite Tiling bug.

--- Assets/Scripts/GUIScript.js
+++ Assets/Scripts/GUIScript.js
@@ -16,6 +16,4 @@ function OnGUI() {
     GUI.BeginGroup(new Rect(50,50,250,300));
 	GUI.Box(Rect(0,0,200,80),"Press up or down to move the bullet and press space to fire. The goal is to hit the target. Enter resets the level.");
 	GUI.EndGroup ();
-	Debug.Log(Screen.width/2-150);
-	Debug.Log(Screen.height/2-150);
 }

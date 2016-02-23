03932f71e295c1edf6322fa991006538e38adeb6
Yuqi Hou <yuqi.hou@gmail.com>
2016-02-23 17:55:52 -0500

    Added menu and credits

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void ClickButton(){
+		Debug.Log ("Clicked");
+		SceneManager.LoadScene ("aimTestScene");
+	}
+		
+}

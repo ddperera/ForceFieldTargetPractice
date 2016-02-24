799ad597e7a2526bd9514c9cd636d1c044b3ece2
Yuqi Hou <yuqi.hou@gmail.com>
2016-02-24 00:12:28 -0500

    Updated target and bullet graphics

--- Assets/Scripts/GUIScript.js
+++ Assets/Scripts/GUIScript.js
@@ -14,6 +14,6 @@ function OnGUI() {
 	GUI.skin = MenuSkin;
 	GUI.skin.box.wordWrap = true;
     GUI.BeginGroup(new Rect(50,50,250,300));
-	GUI.Box(Rect(0,0,200,80),"Press up or down to move the bullet and press space to fire. The goal is to hit the target. Enter resets the level.");
+	GUI.Box(Rect(0,0,200,80),"Press up or down to move the spaceship and press space to fire. The goal is to hit the target. Enter resets the level.");
 	GUI.EndGroup ();
 }
